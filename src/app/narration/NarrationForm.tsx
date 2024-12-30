28114eae9ded1977662c5f813858f7ca85e35c06
thewanionly <pelloani@gmail.com>
2024-12-31 06:39:54 +0800

    feat: implement NarrationForm

---
+++ src/app/narration/NarrationForm.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+
+const NUM_OF_PLAYERS_VALUES = ['5', '6', '7', '8', '9', '10'] as const;
+
+const FormSchema = z.object({
+  numOfPlayers: z.enum(NUM_OF_PLAYERS_VALUES, {
+    required_error: 'You need to select how many players are playing.',
+  }),
+});
+
+export const NarrationForm = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof FormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-2/3 flex-col justify-between space-y-6"
+      >
+        <FormField
+          control={form.control}
+          name="numOfPlayers"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Select number of players</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-wrap space-y-1"
+                >
+                  {NUM_OF_PLAYERS_VALUES.map((value) => (
+                    <FormItem key={value} className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value={value} />
+                      </FormControl>
+                      <FormLabel className="font-normal">{value}</FormLabel>
+                    </FormItem>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="self-start" type="submit">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
