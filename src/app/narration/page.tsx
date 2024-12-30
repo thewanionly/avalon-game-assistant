b78d330345e42a8f194ccc7f39b1d69d7736e016
thewanionly <pelloani@gmail.com>
2024-12-31 06:06:19 +0800

    feat: create narration page

---
+++ src/app/narration/page.tsx
@@ -0,0 +1,7 @@
+export default function Narration() {
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <h1 className="text-2xl">Narration</h1>
+    </div>
+  );
+}
