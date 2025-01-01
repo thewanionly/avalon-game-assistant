e6ed3b00e3d612d67b491c701f1309898d58fe35
thewanionly <pelloani@gmail.com>
2025-01-02 05:35:16 +0800

    refactor: fix typo in basic script

--- src/app/narration/Narration.constants.ts
+++ src/app/narration/Narration.constants.ts
@@ -1,6 +1,6 @@
 export const BASIC_SCRIPT = `
   "Everyone close your eyes and extend your hand into a fist in front of you"
-  "Minions of Mordred oDen your eyes and look around so that you know all agents of Evil"
+  "Minions of Mordred open your eyes and look around so that you know all agents of Evil"
   "Minions of Mordred close your eyes"
   "All players should have their eyes closed and hands in a fist in front of them"
   "Minions of Mordred - extend your thumb so that Merlin will know of you"
