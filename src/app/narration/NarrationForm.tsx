891b998984fcf6ac29b019e7aca65a64dbbb8bac
thewanionly <pelloani@gmail.com>
2025-01-02 05:40:30 +0800

    feat: add narration script logic

--- src/app/narration/NarrationForm.tsx
+++ src/app/narration/NarrationForm.tsx
@@ -31,6 +31,8 @@ interface NarrationFormProps {
   onFormSubmit: (narrationScript: string) => void;
 }
 
+const conditionalString = (condition: boolean, str: string) => (condition ? str : '');
+
 export const NarrationForm = ({ onFormSubmit }: NarrationFormProps) => {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
@@ -40,9 +42,38 @@ export const NarrationForm = ({ onFormSubmit }: NarrationFormProps) => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof FormSchema>) {
-    console.log(values);
-    return onFormSubmit(BASIC_SCRIPT);
+  function onSubmit({ specialCharacters }: z.infer<typeof FormSchema>) {
+    if (!specialCharacters || specialCharacters.length === 0) {
+      onFormSubmit(BASIC_SCRIPT);
+      return;
+    }
+
+    const hasPercival = specialCharacters.includes('Percival');
+    const hasMordred = specialCharacters.includes('Mordred');
+    const hasMorgana = specialCharacters.includes('Morgana');
+    const hasOberon = specialCharacters.includes('Oberon');
+
+    const percivalScript = `
+      "Merlin ${conditionalString(hasMorgana, `and Morgana`)} - extend your thumb so that Percival may know of you"
+      "Percival, open your eyes so you may know Merlin ${conditionalString(hasMorgana, `and Morgana`)}"
+      "Merlin ${conditionalString(hasMorgana, `and Morgana`)} - put your thumbs down and form your hand into a fist"
+      "Percival, close your eyes"
+      "All players should have their eyes closed and hands in a fist in front of them"
+    `;
+
+    onFormSubmit(`
+      "Everyone close your eyes and extend your hand into a fist in front of you"
+      "Minions of Mordred, ${conditionalString(hasOberon, `not Oberon`)} - open your eyes and look around so that you know all agents of Evil"
+      "Minions of Mordred close your eyes"
+      "All players should have their eyes closed and hands in a fist in front of them"
+      "Minions of Mordred, ${conditionalString(hasMordred, `not Mordred himself`)} - extend your thumb so that Merlin will know of you"
+      "Merlin, open your eyes and see the agents of Evil"
+      "Minions of Mordred - put your thumbs down and re-form your hand into a fist"
+      "Merlin, close your eyes"
+      "All players should have their eyes closed and hands in a fist in front of them"
+      ${conditionalString(hasPercival, percivalScript)}
+      "Everyone open your eyes"
+    `);
   }
 
   return (
