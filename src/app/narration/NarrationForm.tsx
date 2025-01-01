f02b07f9f493b226770713dfb1c56cb11292c16c
thewanionly <pelloani@gmail.com>
2025-01-02 05:18:29 +0800

    refactor: use checkbox for the special characters

--- src/app/narration/NarrationForm.tsx
+++ src/app/narration/NarrationForm.tsx
@@ -15,15 +15,16 @@ import {
 } from '@/components/ui/form';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { BASIC_SCRIPT } from './Narration.constants';
+import { Checkbox } from '@/components/ui/checkbox';
 
 const NUM_OF_PLAYERS_VALUES = ['5', '6', '7', '8', '9', '10'] as const;
-const SPECIAL_CHARACTERS_VALUES = ['Percival', 'Mordred', 'Morgana', 'Oberon', 'None'] as const;
+const SPECIAL_CHARACTERS_VALUES = ['Percival', 'Mordred', 'Morgana', 'Oberon'] as const;
 
 const FormSchema = z.object({
   numOfPlayers: z.enum(NUM_OF_PLAYERS_VALUES, {
     required_error: 'You need to select how many players are playing.',
   }),
-  specialCharacters: z.enum(SPECIAL_CHARACTERS_VALUES),
+  specialCharacters: z.array(z.string()).optional(),
 });
 
 interface NarrationFormProps {
@@ -33,6 +34,9 @@ interface NarrationFormProps {
 export const NarrationForm = ({ onFormSubmit }: NarrationFormProps) => {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
+    defaultValues: {
+      specialCharacters: [],
+    },
   });
 
   function onSubmit(values: z.infer<typeof FormSchema>) {
@@ -75,25 +79,37 @@ export const NarrationForm = ({ onFormSubmit }: NarrationFormProps) => {
         <FormField
           control={form.control}
           name="specialCharacters"
-          render={({ field }) => (
+          render={() => (
             <FormItem className="space-y-3">
               <FormLabel>Which of these special characters are included in your game?</FormLabel>
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="flex flex-wrap space-y-1"
-                >
-                  {SPECIAL_CHARACTERS_VALUES.map((value) => (
-                    <FormItem key={value} className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value={value} />
-                      </FormControl>
-                      <FormLabel className="font-normal">{value}</FormLabel>
-                    </FormItem>
-                  ))}
-                </RadioGroup>
-              </FormControl>
+
+              {SPECIAL_CHARACTERS_VALUES.map((value) => (
+                <FormField
+                  key={value}
+                  control={form.control}
+                  name="specialCharacters"
+                  render={({ field }) => {
+                    return (
+                      <FormItem
+                        key={value}
+                        className="flex flex-row items-start space-x-3 space-y-0"
+                      >
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value?.includes(value)}
+                            onCheckedChange={(checked) => {
+                              return checked
+                                ? field.onChange([...(field.value ?? []), value])
+                                : field.onChange(field.value?.filter((v) => v !== value));
+                            }}
+                          />
+                        </FormControl>
+                        <FormLabel className="text-sm font-normal">{value}</FormLabel>
+                      </FormItem>
+                    );
+                  }}
+                />
+              ))}
               <FormMessage />
             </FormItem>
           )}
