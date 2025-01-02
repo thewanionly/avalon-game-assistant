a5fdb559ced0e4ea909d9c40eed9ea0935ab902c
thewanionly <pelloani@gmail.com>
2025-01-03 01:17:30 +0800

    feat: apply text-center to CharacterCard

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -9,7 +9,7 @@ export const CharacterCard = ({ name, isEvil = false }: CharacterCardProps) => {
   return (
     <div
       className={cn(
-        'flex h-[200px] w-[130px] items-center justify-center rounded-md',
+        'flex h-[200px] w-[130px] items-center justify-center rounded-md text-center',
         isEvil ? 'bg-red-500 p-4 text-slate-50' : 'bg-blue-500 p-4 text-neutral-900'
       )}
     >
