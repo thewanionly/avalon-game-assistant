d1c623d72551d35a26416b48dc4203204b482116
thewanionly <pelloani@gmail.com>
2025-01-03 01:01:07 +0800

    feat: implement CharacterCard component

---
+++ src/components/CharacterCard.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils';
+
+interface CharacterCardProps {
+  name: string;
+  isEvil?: boolean;
+}
+
+export const CharacterCard = ({ name, isEvil = false }: CharacterCardProps) => {
+  return (
+    <div
+      className={cn(
+        'flex h-[200px] w-[130px] items-center justify-center rounded-md',
+        isEvil ? 'bg-red-500 p-4 text-slate-50' : 'bg-blue-500 p-4 text-neutral-900'
+      )}
+    >
+      {name}
+    </div>
+  );
+};
