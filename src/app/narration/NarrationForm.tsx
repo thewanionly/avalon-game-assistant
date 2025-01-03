09e457cc6e22a4d9116979c9b380d7bbaeb83ce6
thewanionly <pelloani@gmail.com>
2025-01-04 01:10:19 +0800

    refactor: remove numOfPlayers field

--- src/app/narration/NarrationForm.tsx
+++ src/app/narration/NarrationForm.tsx
@@ -14,7 +14,6 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Checkbox } from '@/components/ui/checkbox';
 import { CharacterCard } from '@/components/CharacterCard';
 import {
@@ -27,13 +26,9 @@ import {
 const CHARACTERS_VALUES = [...GOOD_AVALON_CHARACTERS, ...EVIL_AVALON_CHARACTERS].map(
   ({ name }) => name
 );
-const NUM_OF_PLAYERS_VALUES = ['5', '6', '7', '8', '9', '10'] as const;
 const SPECIAL_CHARACTERS_VALUES = ['Percival', 'Mordred', 'Morgana', 'Oberon'] as const;
 
 const FormSchema = z.object({
-  numOfPlayers: z.enum(NUM_OF_PLAYERS_VALUES, {
-    required_error: 'You need to select how many players are playing.',
-  }),
   specialCharacters: z.array(z.string()).optional(),
   characters: z
     .array(z.string())
@@ -54,7 +49,6 @@ export const NarrationForm = ({ onFormSubmit }: NarrationFormProps) => {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      numOfPlayers: '5',
       specialCharacters: [],
       characters: [
         AvalonCharacterName.Merlin,
@@ -115,32 +109,6 @@ export const NarrationForm = ({ onFormSubmit }: NarrationFormProps) => {
         onSubmit={form.handleSubmit(onSubmit)}
         className="flex flex-col justify-between space-y-6"
       >
-        <FormField
-          control={form.control}
-          name="numOfPlayers"
-          render={({ field }) => (
-            <FormItem className="space-y-3">
-              <FormLabel>Select number of players</FormLabel>
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="flex flex-wrap space-y-1"
-                >
-                  {NUM_OF_PLAYERS_VALUES.map((value) => (
-                    <FormItem key={value} className="flex items-center space-x-3 space-y-0">
-                      <FormControl>
-                        <RadioGroupItem value={value} />
-                      </FormControl>
-                      <FormLabel className="font-normal">{value}</FormLabel>
-                    </FormItem>
-                  ))}
-                </RadioGroup>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
         <FormField
           control={form.control}
           name="specialCharacters"
