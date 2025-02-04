ba015cf975426f9de277b5ac19b9953332ec935e
thewanionly <pelloani@gmail.com>
2025-02-05 06:32:53 +0800

    refactor: update spaces of the script

--- src/helper/generateNarrationScript/generateNarrationScript.ts
+++ src/helper/generateNarrationScript/generateNarrationScript.ts
@@ -16,19 +16,19 @@ export const generateNarrationScript = (options?: NarrationScriptOptions): strin
   } = options ?? {};
 
   const percivalScript = `
-    Merlin ${conditionalString(hasMorgana, `and Morgana`)}, extend your thumb so that Percival may know of you.
-    Percival, open your eyes so you may know Merlin ${conditionalString(hasMorgana, `and Morgana`)}.
-    Merlin ${conditionalString(hasMorgana, `and Morgana`)}, put your thumbs down and form your hand into a fist.
+    Merlin${conditionalString(hasMorgana, ` and Morgana`)}, extend your thumb so that Percival may know of you.
+    Percival, open your eyes so you may know Merlin${conditionalString(hasMorgana, ` and Morgana`)}.
+    Merlin${conditionalString(hasMorgana, ` and Morgana`)}, put your thumbs down and form your hand into a fist.
     Percival, close your eyes.
     All players should have their eyes closed and hands in a fist in front of them.
   `;
 
   const narrationScript = `
     Everyone, close your eyes and extend your hand into a fist in front of you.
-    Minions of Mordred, ${conditionalString(hasOberon, `not Oberon,`)} open your eyes and look around so that you know all agents of Evil.
+    Minions of Mordred,${conditionalString(hasOberon, ` not Oberon,`)} open your eyes and look around so that you know all agents of Evil.
     Minions of Mordred, close your eyes.
     All players should have their eyes closed and hands in a fist in front of them.
-    Minions of Mordred, ${conditionalString(hasMordred, `not Mordred himself,`)} extend your thumb so that Merlin will know of you.
+    Minions of Mordred,${conditionalString(hasMordred, ` not Mordred himself,`)} extend your thumb so that Merlin will know of you.
     Merlin, open your eyes and see the agents of Evil.
     Minions of Mordred, put your thumbs down and re-form your hand into a fist.
     Merlin, close your eyes.
