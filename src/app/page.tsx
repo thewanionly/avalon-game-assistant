d8b8aa317336049e19df9f77c1234fee451004ba
thewanionly <pelloani@gmail.com>
2025-06-12 03:03:35 +0800

    feat: center avalon game assistant text in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8">
-        <h1 className="text-3xl">{APP_TITLE}</h1>
+        <h1 className="text-center text-3xl">{APP_TITLE}</h1>
         <Button asChild>
           <Link href={NARRATION.href}>{NARRATION.linkLabel}</Link>
         </Button>
